0b8c066fd2c2f6039ea7a909a8fa7aab63cd2048
Blaiva <blayverthreyesp14@gmail.com>
2026-05-16 17:32:56 -0400

    Crea UpsertOcupacionUseCaseTest.kt

--- app/src/test/java/edu/ucne/registroocupaciones/data/empleado/EmpleadoRepositoryImplTest.kt
+++ app/src/test/java/edu/ucne/registroocupaciones/data/empleado/EmpleadoRepositoryImplTest.kt
@@ -1,8 +1,7 @@
-package edu.ucne.registroocupaciones.data.ocupacion
+package edu.ucne.registroocupaciones.data.empleado
 
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoDao
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoEntity
-import edu.ucne.registroocupaciones.data.local.ocupacion.OcupacionEntity
 import edu.ucne.registroocupaciones.data.repository.empleado.EmpleadoRepositoryImpl
 import edu.ucne.registroocupaciones.domain.model.empleado.Empleado
 import io.mockk.Runs
@@ -12,14 +11,13 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.slot
+import junit.framework.TestCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import java.time.LocalDate
-import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
-import junit.framework.TestCase.assertNotNull
 
 class EmpleadoRepositoryImplTest {
     private lateinit var dao: EmpleadoDao
@@ -46,12 +44,12 @@ class EmpleadoRepositoryImplTest {
 
         val result = repository.upsert(empleado)
 
-        assertEquals(0, result)
+        TestCase.assertEquals(0, result)
         coVerify { dao.upsert(any()) }
-        assertEquals(LocalDate.now(), empleadoSlot.captured.fechaIngreso)
-        assertEquals("Juan", empleadoSlot.captured.nombres)
-        assertEquals("Masculino", empleadoSlot.captured.sexo)
-        assertEquals(10000.0, empleadoSlot.captured.sueldo)
+        TestCase.assertEquals(LocalDate.now(), empleadoSlot.captured.fechaIngreso)
+        TestCase.assertEquals("Juan", empleadoSlot.captured.nombres)
+        TestCase.assertEquals("Masculino", empleadoSlot.captured.sexo)
+        TestCase.assertEquals(10000.0, empleadoSlot.captured.sueldo)
     }
 
     @Test
@@ -65,9 +63,9 @@ class EmpleadoRepositoryImplTest {
         )
         coEvery { dao.upsert(any()) } just Runs
 
-        val result  = repository.upsert(empleado)
+        val result = repository.upsert(empleado)
 
-        assertEquals(1, result)
+        TestCase.assertEquals(1, result)
         coVerify { dao.upsert(any()) }
     }
 
@@ -84,29 +82,29 @@ class EmpleadoRepositoryImplTest {
     @Test
     fun `observeEmpleados retorna flow de ocupaciones`() = runTest {
         val entities = listOf(
-            EmpleadoEntity(1, LocalDate.now().minusYears(1),"Juan", "Masculino",10000.0),
-            EmpleadoEntity(2, LocalDate.now().minusYears(2),"Maria", "Femenino",8000.0),
+            EmpleadoEntity(1, LocalDate.now().minusYears(1), "Juan", "Masculino", 10000.0),
+            EmpleadoEntity(2, LocalDate.now().minusYears(2), "Maria", "Femenino", 8000.0),
         )
         every { dao.listar() } returns flowOf(entities)
 
         val result = repository.observeEmpleados().first()
 
-        assertEquals(2, result.size)
-        assertEquals("Juan", result[0].nombres)
-        assertEquals("Maria", result[1].nombres)
+        TestCase.assertEquals(2, result.size)
+        TestCase.assertEquals("Juan", result[0].nombres)
+        TestCase.assertEquals("Maria", result[1].nombres)
     }
 
     @Test
-    fun `buscar retorna ocupacion correctamente`() = runTest{
+    fun `buscar retorna ocupacion correctamente`() = runTest {
         val entity = EmpleadoEntity(1, LocalDate.now(), "Juan", "Masculino", 10000.0)
         coEvery { dao.buscar(1) } returns entity
 
         val result = repository.getEmpleado(1)
 
-        assertNotNull(result)
-        assertEquals(LocalDate.now(), result?.fechaIngreso)
-        assertEquals("Juan", result?.nombres)
-        assertEquals("Masculino", result?.sexo)
-        assertEquals(10000.0, result?.sueldo)
+        TestCase.assertNotNull(result)
+        TestCase.assertEquals(LocalDate.now(), result?.fechaIngreso)
+        TestCase.assertEquals("Juan", result?.nombres)
+        TestCase.assertEquals("Masculino", result?.sexo)
+        TestCase.assertEquals(10000.0, result?.sueldo)
     }
 }
