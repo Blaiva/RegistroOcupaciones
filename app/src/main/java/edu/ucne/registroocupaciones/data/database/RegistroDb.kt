874f3c0b65b93252262ebc07339df5842e5d9205
Blaiva <blayverthreyesp14@gmail.com>
2026-05-22 11:01:19 -0400

    Actualiza RegistroDb.kt

--- app/src/main/java/edu/ucne/registroocupaciones/data/database/RegistroDb.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/database/RegistroDb.kt
@@ -6,15 +6,19 @@ import androidx.room.TypeConverters
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoConverters
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoDao
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoEntity
+import edu.ucne.registroocupaciones.data.local.horaextra.HoraExtraConverters
+import edu.ucne.registroocupaciones.data.local.horaextra.HoraExtraDao
+import edu.ucne.registroocupaciones.data.local.horaextra.HoraExtraEntity
 import edu.ucne.registroocupaciones.data.local.ocupacion.OcupacionDao
 import edu.ucne.registroocupaciones.data.local.ocupacion.OcupacionEntity
 
 @Database(
-    entities = [OcupacionEntity::class, EmpleadoEntity::class],
-    version = 2
+    entities = [OcupacionEntity::class, EmpleadoEntity::class, HoraExtraEntity::class],
+    version = 3
 )
-@TypeConverters(EmpleadoConverters::class)
+@TypeConverters(EmpleadoConverters::class, HoraExtraConverters::class)
 abstract class RegistroDb: RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
     abstract fun empleadoDao(): EmpleadoDao
+    abstract fun horaExtraDao(): HoraExtraDao
 }
