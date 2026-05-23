f622ce6fb3dc19d2a7ba88af47f2819c74333090
Blaiva <blayverthreyesp14@gmail.com>
2026-05-23 18:38:24 -0400

    Unifica los Converters.kt

---
+++ app/src/main/java/edu/ucne/registroocupaciones/data/database/Converters.kt
@@ -0,0 +1,39 @@
+package edu.ucne.registroocupaciones.data.database
+
+import androidx.room.TypeConverter
+import edu.ucne.registroocupaciones.data.local.empleado.FrecuenciaPago
+import edu.ucne.registroocupaciones.data.local.horaextra.TipoHoraExtra
+import java.time.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun localDateFromString(value: String?): LocalDate?{
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun localDateToString(date: LocalDate?): String?
+    {
+        return date?.toString()
+    }
+
+    @TypeConverter
+    fun frecuenciaPagoFromString(value: String): FrecuenciaPago {
+        return FrecuenciaPago.valueOf(value)
+    }
+
+    @TypeConverter
+    fun frecuenciaPagoToString(value: FrecuenciaPago): String{
+        return value.name
+    }
+
+    @TypeConverter
+    fun tipoHoraExtraFromString(value: String): TipoHoraExtra {
+        return TipoHoraExtra.valueOf(value)
+    }
+
+    @TypeConverter
+    fun tipoHoraExtraToString(value: TipoHoraExtra): String{
+        return value.descripcion
+    }
+}
--- app/src/main/java/edu/ucne/registroocupaciones/data/database/RegistroDb.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/database/RegistroDb.kt
@@ -3,10 +3,8 @@ package edu.ucne.registroocupaciones.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoConverters
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoDao
 import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoEntity
-import edu.ucne.registroocupaciones.data.local.horaextra.HoraExtraConverters
 import edu.ucne.registroocupaciones.data.local.horaextra.HoraExtraDao
 import edu.ucne.registroocupaciones.data.local.horaextra.HoraExtraEntity
 import edu.ucne.registroocupaciones.data.local.ocupacion.OcupacionDao
@@ -16,7 +14,7 @@ import edu.ucne.registroocupaciones.data.local.ocupacion.OcupacionEntity
     entities = [OcupacionEntity::class, EmpleadoEntity::class, HoraExtraEntity::class],
     version = 3
 )
-@TypeConverters(EmpleadoConverters::class, HoraExtraConverters::class)
+@TypeConverters(Converters::class)
 abstract class RegistroDb: RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
     abstract fun empleadoDao(): EmpleadoDao
