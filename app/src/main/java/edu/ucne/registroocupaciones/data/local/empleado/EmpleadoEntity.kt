8f1de4dbc87d72d5ea819e42ff3fb2940c578615
Blaiva <blayverthreyesp14@gmail.com>
2026-05-23 21:20:59 -0400

    Agrega las llaves foraneas a EmpleadoEntity.kt y HoraExtraEntity.kt

--- app/src/main/java/edu/ucne/registroocupaciones/data/local/empleado/EmpleadoEntity.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/local/empleado/EmpleadoEntity.kt
@@ -1,10 +1,23 @@
 package edu.ucne.registroocupaciones.data.local.empleado
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import edu.ucne.registroocupaciones.data.local.ocupacion.OcupacionEntity
 import java.time.LocalDate
 
-@Entity(tableName = "empleados")
+@Entity(tableName = "empleados",
+    foreignKeys = [
+        ForeignKey(
+            entity = OcupacionEntity::class,
+            parentColumns = ["ocupacionId"],
+            childColumns = ["ocupacionId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("ocupacionId")]
+)
 data class EmpleadoEntity(
     @PrimaryKey(autoGenerate = true)
     val empleadoId: Int,
--- app/src/main/java/edu/ucne/registroocupaciones/data/local/horaextra/HoraExtraEntity.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/local/horaextra/HoraExtraEntity.kt
@@ -1,10 +1,24 @@
 package edu.ucne.registroocupaciones.data.local.horaextra
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import edu.ucne.registroocupaciones.data.local.empleado.EmpleadoEntity
 import java.time.LocalDate
 
-@Entity(tableName = "horas_extras")
+@Entity(
+    tableName = "horas_extras",
+    foreignKeys = [
+        ForeignKey(
+            entity = EmpleadoEntity::class,
+            parentColumns = ["empleadoId"],
+            childColumns = ["empleadoId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("empleadoId")]
+)
 data class HoraExtraEntity(
     @PrimaryKey(autoGenerate = true)
     val horaExtraId: Int,
